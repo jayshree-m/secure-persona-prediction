b4297ac2267f01967417266386e0625d4b11fb44
jayshree-m <jayshreemahajan2406@gmail.com>
2026-09-10 23:55:49 +0530

    Working

---
+++ frontend/js/script.js
@@ -0,0 +1,21 @@
+function startScanning() {
+
+    alert("Privacy Scanner will be available soon!");
+
+}
+
+
+function learnMore() {
+
+    document.getElementById("features").scrollIntoView({
+        behavior: "smooth"
+    });
+
+}
+
+
+function goToLogin() {
+
+    alert("Login system will be added next!");
+
+}
